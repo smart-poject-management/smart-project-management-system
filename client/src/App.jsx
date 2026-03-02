628f9aa439537d274e242fb41157b7dd416e9ba9
Gaurav Tomar <gauravtomar977@gmail.com>
2026-03-02 10:46:48 +0530

    change path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,6 +40,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         {/* Auth Routes */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
