d852032e8cbece341c6b205f6342275305058b31
Gaurav Tomar <gauravtomar977@gmail.com>
2026-03-16 22:21:53 +0530

    fix bug

--- server/services/fileServices.js
+++ server/services/fileServices.js
@@ -1,26 +1,31 @@
 import fs from "fs";
 import ErrorHandler from "../middlewares/error.js";
+
 export const streamDownload = (fileUrl, res, originalName) => {
   try {
-    if (fs.existsSync(fileUrl)) {
+    if (!fs.existsSync(fileUrl)) {
       throw new ErrorHandler("File not found", 404);
     }
+
     res.download(fileUrl, originalName, (err) => {
       if (err) {
         console.error("Error downloading file:", err);
-        throw new ErrorHandler("Error downloading file", 500);
+
+        if (!res.headersSent) {
+          res.status(500).json({
+            success: false,
+            message: "Error downloading file",
+          });
+        }
       }
     });
+
   } catch (error) {
-    if (error instanceof ErrorHandler) {
-      return res.status(error.statusCode).json({
+    if (!res.headersSent) {
+      res.status(error.statusCode || 500).json({
         success: false,
-        message: error.message,
+        message: error.message || "Internal Server Error",
       });
     }
-    res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
   }
 };
